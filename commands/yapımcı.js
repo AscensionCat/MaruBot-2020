b5964c65167aa48599cbe3757dbe118b9b252b61
Glitch (marulbot) <none>
2020-05-31 15:24:52 +0000

    Before import of AscensionCat/Marubot without specified path

--- commands/yapımcı.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    message.channel.send("AscensionCat#3162  <@287210984723185666>  ID:287210984723185666")
-    }
-
-module.exports.help = {
-    name: "yapımcı"
-}
